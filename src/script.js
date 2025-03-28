db9d292fb4330ffd8e572d5c34f8eff892f31b75
Axel <axel.krantz@skola.taby.se>
2025-03-28 23:54:42 +0100

    added sprite animation

--- src/script.js
+++ src/script.js
@@ -7,9 +7,6 @@ var uiHeight=50
 canvas.width=width*res
 canvas.height=uiHeight+height*res
 
-var img=document.createElement('img')
-img.src='img/earth.png'
-
 var then,now,elapsed
 var fpsint=260
 
@@ -19,19 +16,35 @@ var pos={x:0,y:7}
 var dir={x:1,y:0}
 
 class Earth{
-    constructor(canvasWidth, canvasHeight){
-        this.canvasWidth=canvasWidth
-        this.canvasHeight=canvasHeight
-        this.image=document
-
+    constructor(src,spriteWidth, spriteHeight, x, y){
+        this.image=document.createElement('img')
+        this.image.src=src
+        this.spriteWidth=spriteWidth
+        this.spriteHeight=spriteHeight
+        this.scale=32
+        this.x=x
+        this.y=y
+        this.minFrame=0
+        this.maxFrame=63
+        this.frame=0
+        this.frameX=0
+        this.frameY=0
+    }
+    draw(){
+        ctx.drawImage(
+            this.image, this.frameX*this.spriteWidth, this.frameY*this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x*res,this.y*res+uiHeight,32,32
+        )
+    }
+    update(){
+        this.frame = this.frame < this.maxFrame ? this.frame + 1 : this.minFrame
+        this.frameX = this.frame % (this.image.width/this.spriteWidth)
+        this.frameY = Math.floor(this.frame/this.image.height*this.spriteHeight)
     }
 }
 
-
-function run(){
-    move()
-    draw()
-}
+var e=[]
+e.push(new Earth('img/earth.png',128,128, 0, 0))
+e.push(new Earth('img/earth.png',128,128, 0, 0))
 
 function draw(){
     ctx.clearRect(0,0,canvas.width,canvas.height)
@@ -44,11 +57,14 @@ function draw(){
         drawBoard(i%width*res,Math.floor(i/width)*res,res,res)
     }
 
-    ctx.drawImage(img, 0, 0, 128, 128,0*res,0*res+uiHeight,32,32)
-
     ctx.fillStyle = 'black'
     for(let i = 0; i < arr.length; i++){
         drawBoard(arr[i].x*res,arr[i].y*res,res,res)
+    }
+
+    for(let i = 0; i < e.length; i++){
+        e[i].draw()
+        e[i].update()
     }
 }
 
@@ -85,9 +101,8 @@ function keydown(key){
 window.addEventListener('load', function(){
     document.addEventListener('keydown', function(e){keydown(e.key)})
     then=Date.now()
-    draw()
     update()
-});
+})
 
 //kör run() vid fpsint millisekunder
 function update(){
@@ -101,6 +116,8 @@ function update(){
         //funktionen körs 60fps så elapsed får inte vara en faktor av 60fps
         then=now-(elapsed%fpsint)
 
-        run()
+        move()
     }
+
+    draw()
 }
